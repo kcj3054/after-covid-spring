0d4b2c6432e2a8912d278b8c3db2ba5513fafc3e
Abeeeeeeeee9 <87168569+Abeeeeeeeee9@users.noreply.github.com>
2021-11-21 21:16:00 +0900

    Update detail.java

--- src/main/java/com/example/aftercovidversion2/controller/detail.java
+++ src/main/java/com/example/aftercovidversion2/controller/detail.java
@@ -1,4 +1,15 @@
-package com.example.aftercovidversion2;
+package com.example.aftercovidversion2.controller;
 
-public class detail {
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class DetailController {
+
+    @GetMapping("/detail")
+    public String getPageDetail(){
+        return "detail";
+    }
 }
